f8dd5ebb0f3e69a67fe72a1bc3d7d730324d41a1
JoldoshAjimudunov <joldoshbek8856@gmail.com>
2023-02-13 12:32:31 +0600

    develop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import BurgerMenu from "./components/burgermenu/BurgerMenu";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BurgerMenu />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/burgermenu/BurgerMenu.jsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Fade from "@mui/material/Fade";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import cl from "./Burger.module.css";
+
+const BurgerMenu = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={cl.bur_container}>
+      <Button
+        id="fade-button"
+        aria-controls={open ? "fade-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon className={cl.burger} />
+        </IconButton>
+      </Button>
+      <Menu
+        className={cl.bur__val}
+        id="fade-menu"
+        MenuListProps={{
+          "aria-labelledby": "fade-button",
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default BurgerMenu;
